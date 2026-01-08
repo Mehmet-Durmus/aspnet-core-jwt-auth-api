dfe4ef942625669c6589fe7d0b4d0de32d5551b3
MehmetDrms <mehmet.drms@outlook.com>
2026-01-08 13:01:24 +0300

    Entities and DbContext

---
+++ LogInSignUp.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using LogInSignUp.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogInSignUp.DataAccess.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity => 
+            {
+                entity.HasIndex(x => x.UserName).IsUnique();
+                entity.HasIndex(x => x.Email).IsUnique();
+            });
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entities = ChangeTracker.Entries<BaseEntity>();
+            foreach (var entity in entities)
+            {
+                switch (entity.State)
+                {
+                    case EntityState.Added:
+                        entity.Entity.CreatedAt = DateTime.UtcNow;
+                        entity.Entity.IsActive = true;
+                        break;
+                    case EntityState.Modified:
+                        entity.Entity.UpdatedAt = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            var users = ChangeTracker.Entries<User>();
+            foreach (var user in users)
+            {
+                switch (user.State)
+                {
+                    case EntityState.Added:
+                        user.Entity.IsEmailVerified = false;
+                        break;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ LogInSignUp.DataAccess/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogInSignUp.DataAccess.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public string EmailVerificationToken { get; set; }
+        public DateTime EmailVerificationTokenEndDate { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordResetToken { get; set; }
+        public DateTime PasswordResetTokenEndDate { get; set; }
+        public bool IsPasswordResetTokenUsed { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenEndDate { get; set; }
+    }
+}
